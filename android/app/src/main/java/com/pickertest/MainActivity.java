a0ef3e34db9e7ec7e82b7003056855b52d2f8e45
yrjwcharm <xlyanrui@sina.com>
2019-03-20 13:36:43 +0800

    Initial commit

---
+++ android/app/src/main/java/com/pickertest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pickertest;
+
+import com.facebook.react.ReactActivity;
+import com.beefe.picker.PickerViewPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "PickerTest";
+    }
+}
